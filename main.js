a66b2b217f4588f421bc6574465a44c33cbb5317
김원형 <dnjsgud0828@gmail.com>
2024-06-04 17:07:28 +0900

    Add Command Pattern

--- main.js
+++ main.js
@@ -16,9 +16,11 @@ class User{
     addInterest(product) { //관심목록 추가
         this.product = product;
     }
+
     update(product) { //바로 작동은 못시킴...?
         console.log(`${product}의 가격이 변경되었습니다!`);
     }
+
     sellProduct(name, price){
        const a = new product(name, price);
         this.sellingList.push(a);
@@ -26,6 +28,7 @@ class User{
        //해당 제품에 대한 설명
        //사진 추가
     }
+
     changePrice(price){
         const a= this.sellingList[0]
         a.discount(price);
@@ -55,72 +58,71 @@ class product{ //구매 글을 올릴 때 해당 클래스의 인스턴스를 �
 
     discount(price){
         this.price-=price;
-        console.log(this.observers);
         this.notify();
     }
 }
 
-// class category {
-//     setWeapon(weapon) {
-//         this.weapon = weapon;
-//     };
+class System { //목록 출력을 명령어 패턴을 사용하여 작성
+    setCategoryCommand(categoryCommand){
+        this.categoryCommand = categoryCommand;
+    }
 
-//     showCategory() { 
-//         this.weapon.fire();
-//     };
-// };
+    show(){
+        this.categoryCommand.showCategory();
+    }
+}
 
-// class clothes {
-//     showCategory() {
-//         console.log('Fire a missile.');
-//     };
-// };
+class Command {
+    showClothes() {
+        console.log();
+    };
 
-// class Bomb7 {
-//     showCategory() {
-//         console.log('Fire a bomb.');
-//     };
-// };
+    showShoes() { 
+        console.log();
+    };
+
+    showToys() {
+        console.log("-----------------------------\n1. robot\n2. doll\n3. 로보카 폴리\n------------------------------");
+    }
+
+};
+
+class Clothes {
+    constructor(){
+        this.category = new Command();
+    }
+
+    showCategory(){
+        this.category.showClothes();
+    }
+};
+
+class Shoes {
+    constructor(){
+        this.category = new Command();
+    }
+
+    showCategory(){
+        this.category.showShoes();
+    }
+};
+
+class Toy {
+    constructor(){
+        this.category = new Command();
+    }
+
+    showCategory(){
+        this.category.showToys();
+    }
+}
 
 //main 실행문
+console.log("------------콜라비-----------");
+const sys = new System();
+sys.setCategoryCommand(new Toy());
+console.log('-----------------------------\n1. 중고거래 \n2. 예약 \n3. 인원 모집\n-----------------------------');
+sys.show();
 const user = new User('원형', 36.5, 'Namyangju');
 user.sellProduct('robot', 15000);
 user.changePrice(5000);
-/*당근
-
-
-// => 홈메뉴 보여주기(command), 판매물품 목록, 지역설정 
-
-명령어 패턴: 
-전략 패턴: 
-옵저버 패턴: 가격내림시 좋아요를 한 회원에게 메세지가 감
-팩토리 패턴
-
-글 작성 => 어떤 분야에 따라서 다르게, 거래/모임/예약/후기: 전략패턴
-가격이 내려갔을 경우: 옵저버 패턴
-메소드 실행시 notify 실행
-다른 객체의 메소드 실행을 notify 하는법
-좋아요 버튼을 눌렀을 경우 소비자를 옵저버 아님 메소드
-
-class User
-{
-    constructor(){
-
-    }
-    가격 내리기(){
-        this.observers.notify()
-    }
-    notify(){
-        console.log("가격이 내려갔습니다!");
-    }
-}
-
-클래스 물품{
-    update(){
-        console.log("가격이 내려갔습니다!");
-    }
-}
-
-사용자에게 콘솔 목록 보여주기 => 명령어 패턴
-
-*/
