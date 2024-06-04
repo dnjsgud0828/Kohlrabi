0f2ec746629f7f02fe8796271eec29d733b7fe77
김원형 <dnjsgud0828@gmail.com>
2024-06-04 15:25:37 +0900

    Add Obserever pattern

--- main.js
+++ main.js
@@ -1,58 +1,126 @@
 class User{
-    constructor(name, mannerTemp, purchaseList, sellingList, chatList, residence){
+    constructor(name, mannerTemp, residence){
         this.name =name;
         this.mannerTemp = mannerTemp;
-        this.sellingList = sellingList;
-        this.purchaseList = purchaseList
-        this.chatList = chatList;
+        this.sellingList = []; //판매물품
+        this.purchaseList = []; //구매목록
+        this.chatList = []; //채팅목록
         this.residence = residence;
-        //판매물품, 채팅목록 구현을 어떻게 해야?
     }
 
-    name(params) {
-        
+    //판매 물품 등록
+    //물품 구매
+    //모임 등록
+    //매장 예약
+
+    addInterest(product) { //관심목록 추가
+        this.product = product;
+    }
+    update(product) { //바로 작동은 못시킴...?
+        console.log(`${product}의 가격이 변경되었습니다!`);
+    }
+    sellProduct(name, price){
+       const a = new product(name, price);
+        this.sellingList.push(a);
+        a.attach(this);
+       //해당 제품에 대한 설명
+       //사진 추가
+    }
+    changePrice(price){
+        const a= this.sellingList[0]
+        a.discount(price);
     }
 }
 
-class category {
-    setWeapon(weapon) {
-        this.weapon = weapon;
-    };
+//구매기능
+class product{ //구매 글을 올릴 때 해당 클래스의 인스턴스를 전달
+    constructor(product, price){
+        this.product = product; //상품명
+        this.price = price; //가격
+        this.state = 0; //0이 디폴트(0: 판매중, 1: 거래 완료, 2: 거래 진행중)
+        this.observers=[]; 
+    }
 
-    showCategory() { 
-        this.weapon.fire();
-    };
-};
+    attach(observer) {
+        this.observers.push(observer);
+    }
 
-class clothes {
-    showCategory() {
-        console.log('Fire a missile.');
-    };
-};
+    detach(observer) {
+        this.observers = this.observers.filter(e => e !== observer);
+    }
 
-class Bomb7 {
-    showCategory() {
-        console.log('Fire a bomb.');
-    };
-};
+    notify(){
+        this.observers.forEach(e=> e.update(this.product)); //물품이 감시하고 있는 유저들에게 알림
+    }
 
-//당근
-//1. 물품 구매 2. 모임 구하기 3. 장소(미용실, 음식점...) 예약
-/* 1. 중고 거래
-처음 들어오면 자기 지역의 판매 물품들을 보여줌
-물품 선택시 좋아요를 설정할 수 있고 관심있는 상품으로 넘어감
-관심있는 상품의 경우 판매자가 가격을 내리거나 판매를 중지할 경우 메세지가 날라감
-판매자와 채팅을 시작 
+    discount(price){
+        this.price-=price;
+        console.log(this.observers);
+        this.notify();
+    }
+}
+
+// class category {
+//     setWeapon(weapon) {
+//         this.weapon = weapon;
+//     };
+
+//     showCategory() { 
+//         this.weapon.fire();
+//     };
+// };
+
+// class clothes {
+//     showCategory() {
+//         console.log('Fire a missile.');
+//     };
+// };
+
+// class Bomb7 {
+//     showCategory() {
+//         console.log('Fire a bomb.');
+//     };
+// };
+
+//main 실행문
+const user = new User('원형', 36.5, 'Namyangju');
+user.sellProduct('robot', 15000);
+user.changePrice(5000);
+/*당근
+
 
 // => 홈메뉴 보여주기(command), 판매물품 목록, 지역설정 
-// 유저정보: 닉네임, 성별, 온도   
 
-const category = new category();
-weapon7.setWeapon(new Missile7()); 
-weapon7.showCategory(); 
-weapon7.setWeapon(new Bomb7());
-weapon7.showCategory(); 
+명령어 패턴: 
+전략 패턴: 
+옵저버 패턴: 가격내림시 좋아요를 한 회원에게 메세지가 감
+팩토리 패턴
 
-//명령어 패턴: 
-//전략 패턴:
-//옵저버 패턴: 가격내림시 좋아요를 한 회원에게 메세지가 감*/
+글 작성 => 어떤 분야에 따라서 다르게, 거래/모임/예약/후기: 전략패턴
+가격이 내려갔을 경우: 옵저버 패턴
+메소드 실행시 notify 실행
+다른 객체의 메소드 실행을 notify 하는법
+좋아요 버튼을 눌렀을 경우 소비자를 옵저버 아님 메소드
+
+class User
+{
+    constructor(){
+
+    }
+    가격 내리기(){
+        this.observers.notify()
+    }
+    notify(){
+        console.log("가격이 내려갔습니다!");
+    }
+}
+
+클래스 물품{
+    update(){
+        console.log("가격이 내려갔습니다!");
+    }
+}
+
+사용자에게 콘솔 목록 보여주기 => 명령어 패턴
+
+*/
---
+++ test.js
@@ -0,0 +1,9 @@
+class a {
+    constructor(a, b, c){
+        this.a =a;
+        this.b =b;
+        this.c =c;
+    }
+}
+const sample = new a(1, 2, 3);
+console.log(sample);
